9694f9e2ba387336d4a0c0128a21c334cea8c3c2
zhangjmjh <zhangjm.jh@qq.com>
2020-11-11 22:23:36 +0800

    js基础

--- test/tool.js
+++ test/tool.js
@@ -5,8 +5,6 @@ function getStyle(node, cssStyle) {
 }
 
 
-
-
 function elementsByClassName(node, classStr) {
     var nodes = node.getElementsByTagName('*');
     var arr = []; //用于存放符合条件的节点
@@ -16,4 +14,15 @@ function elementsByClassName(node, classStr) {
         }
     }
     return arr;
+}
+
+// JS获取随机颜色函数，备用
+//随机颜色，十六进制方法；
+function getRandomColor() {
+    var rand = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    if (rand.length == 6) {
+        return rand;
+    } else {
+        return getRandomColor();
+    }
 }
